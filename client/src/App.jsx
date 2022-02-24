069ccbdf3834e26ddab3ef9aff8a834e1cbb42db
Benjamin Cash <31261400+benjaminjcash@users.noreply.github.com>
2022-02-24 00:49:03 -0700

    Mobile UI (#5)
    * login and register pages
    
    * tweaking start script
    
    Co-authored-by: Benjamin Cash <benjamin.cash@zayo.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,72 +4,60 @@ import { useStyletron } from 'baseui';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { checkLoggedIn } from './actions/auth';
 import { fetchUser } from './actions/user';
-import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
 import ConnectedLogin from './components/Login';
 import Register from './components/Register';
 import Main from './components/Main';
 
 function App({ auth, checkLoggedIn, user, fetchUser }) {
-    const [css, theme] = useStyletron();
-    const [loggedIn, setLoggedIn] = React.useState(auth?.loggedIn);
-    const [currentUser, setCurrentUser] = React.useState(user);
+  const [css, theme] = useStyletron();
+  const [loggedIn, setLoggedIn] = React.useState(auth?.loggedIn);
+  const [currentUser, setCurrentUser] = React.useState(user);
 
-    React.useEffect(() => {
-      checkLoggedIn();
-    }, []);
+  React.useEffect(() => {
+    checkLoggedIn();
+  }, []);
 
-    React.useEffect(() => {
-        setLoggedIn(auth?.loggedIn);
-        if(auth?.loggedIn) fetchUser();
-    }, [auth]);
+  React.useEffect(() => {
+    setLoggedIn(auth?.loggedIn);
+    if(auth?.loggedIn) fetchUser();
+  }, [auth]);
 
-    React.useEffect(() => {
-      setCurrentUser(user);
-    }, [user])
+  React.useEffect(() => {
+    setCurrentUser(user);
+  }, [user])
 
-    return (
-      <Router>
-          <Switch>
-            <Route exact path="/">
-              {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
-            </Route>
-            <Route path="/login">
-              {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
-              <FlexGrid className={css({
-                  	width: '35%',
-                    height: '35%',
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    margin: 'auto'
-              })}>
-                <FlexGridItem className={css({ display: 'flex' })}>
-                  <ConnectedLogin />
-                  <Register />
-                </FlexGridItem>
-              </FlexGrid>
-            </Route>
-            <Route path="/my/fridge">
-              {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
-              <Main currentUser={currentUser}/>
-            </Route>
-          </Switch>
-      </Router>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
+          <ConnectedLogin />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/my/fridge">
+          {loggedIn ? <Redirect to="/my/fridge" /> : <Redirect to="/login" />}
+          <Main currentUser={currentUser}/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 const ConnectedApp = connect(
-    (state) => {
-        return {
-          auth: state.auth,
-          user: state.user
-        }
-    }, {
-        checkLoggedIn,
-        fetchUser
+  (state) => {
+    return {
+      auth: state.auth,
+      user: state.user
     }
+  }, {
+    checkLoggedIn,
+    fetchUser
+  }
 )(App);
 
 export default ConnectedApp;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FlexGrid, FlexGridItem} from 'baseui/flex-grid';
 import {useStyletron} from 'baseui';
 import {Tag, VARIANT} from 'baseui/tag';
@@ -9,66 +9,68 @@ import Fridge from './Fridge';
 import Manage from './Manage';
 
 function Main({ currentUser }) {
-    const [css, $theme] = useStyletron();
-    
-    const [mainItems, setMainItems] = React.useState([
-        { 
-            label: "My Fridge",
-            value: "myFridge",
-            active: true
-        },
-        {
-            label: "Manage",
-            value: "manage"
-        }
-    ]);
-    const userItems = [
-        {label: 'Logout', value: 'logout'}
-    ];
-
-    const renderActiveComponent = () => {
-        const active = mainItems.filter(item => item.active);
-        const activeValue = active[0].value;
-        if(activeValue == 'myFridge') return <Fridge />
-        if(activeValue == 'manage') return <Manage />
+  const [css, $theme] = useStyletron();
+  
+  const [mainItems, setMainItems] = React.useState([
+    { 
+      label: "My Fridge",
+      value: "myFridge",
+      active: true
+    },
+    {
+      label: "Manage",
+      value: "manage"
     }
+  ]);
+  const userItems = [
+    {label: 'Logout', value: 'logout'}
+  ];
 
-    const handleUserItemSelect = (item) => {
-        if(item.value == 'logout') {
-            clearStorage('loggedIn');
-            clearStorage('access_token');
-            clearStorage('refresh_token');
-            window.location.href = '/';
-        }
+  const renderActiveComponent = () => {
+    const active = mainItems.filter(item => item.active);
+    const activeValue = active[0].value;
+    if(activeValue == 'myFridge') return <Fridge />
+    if(activeValue == 'manage') return <Manage />
+  }
+
+  const handleUserItemSelect = (item) => {
+    if(item.value == 'logout') {
+      clearStorage('loggedIn');
+      clearStorage('access_token');
+      clearStorage('refresh_token');
+      window.location.href = '/';
     }
-    
-    return (
-        <FlexGrid className={css({width: '100%'})}>
-            <FlexGridItem>
-                <AppNavBar
-                    title={<Block className={css({ fontSize: $theme.sizing.scale700 })}>Fridge Inventory Tracker</Block>}
-                    mainItems={mainItems}
-                    onMainItemSelect={item => {
-                        setMainItems(prev => setItemActive(prev, item));
-                    }}
-                    userItems={userItems}
-                    username={currentUser.username}
-                    usernameSubtitle={currentUser.name}
-                    onUserItemSelect={handleUserItemSelect}
-                    overrides={{
-                        MainMenuItem: {
-                            style: ({ $theme }) => ({
-                              fontSize: $theme.sizing.scale550
-                            })
-                        }
-                    }}
-                />
-            </FlexGridItem>
-            <FlexGridItem className={css({ justifyContent: 'center'})}>
-                {renderActiveComponent()}
-            </FlexGridItem>
-        </FlexGrid>
-    );
+  }
+
+  return (
+    <>
+    <FlexGrid className={css({width: '100%'})}>
+      <FlexGridItem>
+        <AppNavBar
+          title={<Block className={css({ fontSize: $theme.sizing.scale700 })}>Fridge Inventory Tracker</Block>}
+          mainItems={mainItems}
+          onMainItemSelect={item => {
+            setMainItems(prev => setItemActive(prev, item));
+          }}
+          userItems={userItems}
+          username={currentUser.username}
+          usernameSubtitle={currentUser.name}
+          onUserItemSelect={handleUserItemSelect}
+          overrides={{
+            MainMenuItem: {
+              style: ({ $theme }) => ({
+                fontSize: $theme.sizing.scale550
+              })
+            }
+          }}
+        />
+      </FlexGridItem>
+      <FlexGridItem className={css({ justifyContent: 'center'})}>
+        {renderActiveComponent()}
+      </FlexGridItem>
+    </FlexGrid>
+    </>
+  );
 }
 
 export default Main;
