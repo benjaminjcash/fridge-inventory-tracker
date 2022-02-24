3176708e05f865c17ba64ffb48a4d7d5ddb15ee4
Benjamin Cash <benjamin.cash@zayo.com>
2022-02-23 23:52:48 -0700

    login and register pages

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { useStyletron } from 'baseui';
 import { Card } from 'baseui/card';
 import { Input, SIZE as inputSize } from 'baseui/input';
@@ -10,124 +11,138 @@ import { dispatchError } from '../actions/error';
 import Error from './Error';
 
 const Login = ({ error, requestLogin, dispatchError }) => {
-    const [css, theme] = useStyletron();
+  const [css, theme] = useStyletron();
+  let history = useHistory();
 
-    const [formValues, setFormValues] = React.useState({
+  const [formValues, setFormValues] = React.useState({
+    username: '',
+    password: ''
+  });
+  const [formErrors, setFormErrors] = React.useState({
+    username: false,
+    password: false
+  });
+
+  const navigateRegister = () => {
+    history.push('/register');
+  }
+
+  const submitLogin = () => {
+    let fields = Object.keys(formValues);
+    let errors = {};
+    let hasErrors = false;
+    for (const field of fields) {
+      if (formValues[field].length == 0) {
+        errors[field] = true;
+        hasErrors = true;
+      }
+    }
+    if (hasErrors) {
+      setFormErrors({
+        ...formErrors,
+        ...errors
+      });
+      dispatchError("empty fields");
+    } else {
+      return requestLogin({
+        username: formValues.username,
+        password: formValues.password
+      });
+    }
+  }
+
+  React.useEffect(() => {
+    if (error.message == "wrong password") {
+      setFormValues({
+        ...formValues,
+        password: ''
+      });
+      setFormErrors({
+        ...formErrors,
+        password: true
+      });
+    }
+    if (error.message == "no user found") {
+      setFormValues({
         username: '',
         password: ''
-    });
-    const [formErrors, setFormErrors] = React.useState({
-        username: false,
-        password: false
-    });
-
-    const submitLogin = () => {
-        let fields = Object.keys(formValues);
-        let errors = {};
-        let hasErrors = false;
-        for (const field of fields) {
-            if (formValues[field].length == 0) {
-                errors[field] = true;
-                hasErrors = true;
-            }
-        }
-        if (hasErrors) {
-            setFormErrors({
-                ...formErrors,
-                ...errors
-            });
-            dispatchError("empty fields");
-        } else {
-            return requestLogin({
-                username: formValues.username,
-                password: formValues.password
-            });
-        }
+      });
+      setFormErrors({
+        ...formErrors,
+        username: true
+      });
     }
+    if (!error.error) {
+      setFormErrors({
+        password: false,
+        username: false
+      });
+    }
+  }, [error]);
 
-    React.useEffect(() => {
-        if (error.message == "wrong password") {
-            setFormValues({
+  return (
+    <>
+      {
+        <Card
+          className={css({ width: '100%', height: '100%' })}
+        >
+          <FormControl label="Username">
+            <Input
+              value={formValues.username}
+              onChange={e => setFormValues({
                 ...formValues,
-                password: ''
-            });
-            setFormErrors({
-                ...formErrors,
-                password: true
-            });
-        }
-        if (error.message == "no user found") {
-            setFormValues({
-                username: '',
-                password: ''
-            });
-            setFormErrors({
-                ...formErrors,
-                username: true
-            });
-        }
-        if (!error.error) {
-            setFormErrors({
-                password: false,
-                username: false
-            });
-        }
-    }, [error]);
-
-    return (
-        <>
-            {
-                <Card
-                    className={css({ width: '50%' })}
-                >
-                    <FormControl label="Username">
-                        <Input
-                            value={formValues.username}
-                            onChange={e => setFormValues({
-                                ...formValues,
-                                username: e.target.value
-                            })}
-                            placeholder="Username"
-                            clearOnEscape
-                            error={formErrors.username}
-                            size={inputSize.mini}
-                        />
-                    </FormControl>
-                    <FormControl label="Password">
-                        <Input
-                            value={formValues.password}
-                            onChange={e => setFormValues({
-                                ...formValues,
-                                password: e.target.value
-                            })}
-                            placeholder="Password"
-                            type="password"
-                            clearOnEscape
-                            error={formErrors.password}
-                            size={inputSize.mini}
-                        />
-                    </FormControl>
-                    <Button
-                        onClick={() => submitLogin()}
-                        size={buttonSize.mini}
-                    >Login</Button>
-                </Card>
-            }
-            <Error />
-        </>
-    )
+                username: e.target.value
+              })}
+              placeholder="Username"
+              clearOnEscape
+              error={formErrors.username}
+              size={inputSize.mini}
+            />
+          </FormControl>
+          <FormControl label="Password">
+            <Input
+              value={formValues.password}
+              onChange={e => setFormValues({
+                ...formValues,
+                password: e.target.value
+              })}
+              placeholder="Password"
+              type="password"
+              clearOnEscape
+              error={formErrors.password}
+              size={inputSize.mini}
+            />
+          </FormControl>
+          <Button
+            onClick={() => submitLogin()}
+            size={buttonSize.mini}
+            overrides={{
+              BaseButton: { 
+                style: ({ $theme }) => ({ marginRight: '8px' })
+              }
+            }}
+          >Login</Button>
+          <Button
+            onClick={() => navigateRegister()}
+            size={buttonSize.mini}
+          >Register</Button>
+        </Card>
+      }
+      <Error />
+    </>
+  )
 }
 
 const ConnectedLogin = connect(
-    (state) => {
-        return {
-            error: state.error
-        }
-    },
-    {
-        requestLogin,
-        dispatchError
+  (state) => {
+    return {
+      error: state.error
     }
+  },
+  {
+    requestLogin,
+    dispatchError
+  }
 )(Login);
 
 export default ConnectedLogin;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { useStyletron } from 'baseui';
 import { Card } from 'baseui/card';
 import { Input, SIZE as inputSize } from 'baseui/input';
@@ -11,181 +12,193 @@ import { dispatchError } from '../actions/error';
 import Error from './Error';
 
 const Register = ({ error, dispatchError }) => {
-    const [css, theme] = useStyletron();
+  const [css, theme] = useStyletron();
+  let history = useHistory();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [formValues, setFormValues] = React.useState({
+    name: '',
+    email: '',
+    username: '',
+    password: ''
+  });
+  const [formErrors, setFormErrors] = React.useState({
+    name: false,
+    email: false,
+    username: false,
+    password: false
+  });
 
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [formValues, setFormValues] = React.useState({
+  const closeModal = () => {
+    setIsOpen(false);
+    navigateLogin();
+  }
+
+  const submitRegister = () => {
+    let fields = Object.keys(formValues);
+    let errors = {};
+    let hasErrors = false;
+    for (const field of fields) {
+      if (formValues[field].length == 0) {
+        errors[field] = true;
+        hasErrors = true;
+      }
+    }
+    if (hasErrors) {
+      setFormErrors({
+        ...formErrors,
+        ...errors
+      });
+      dispatchError("empty fields");
+    } else {
+      setFormValues({
         name: '',
         email: '',
         username: '',
         password: ''
-    });
-    const [formErrors, setFormErrors] = React.useState({
+      });
+      const data = {
+        username: formValues.username,
+        password: formValues.password,
+        name: formValues.name,
+        email: formValues.email
+      }
+      axios.post('/api/auth/register', data).then((res) => {
+        if (res?.data?.success) {
+          setIsOpen(true);
+        } else {
+          dispatchError("account already exists");
+        }
+      }).catch((err) => {
+        console.error(err);
+      });
+    }
+  }
+
+  const navigateLogin = () => history.push('/login');
+
+  React.useEffect(() => {
+    if (error.message == "account already exists") {
+      setFormValues({
+        name: '',
+        email: '',
+        username: '',
+        password: ''
+      });
+    }
+    if (!error.error) {
+      setFormErrors({
         name: false,
         email: false,
-        username: false,
-        password: false
-    });
-
-    const closeModal = () => {
-        setIsOpen(false);
+        password: false,
+        username: false
+      });
     }
+  }, [error]);
 
-    const submitRegister = () => {
-        let fields = Object.keys(formValues);
-        let errors = {};
-        let hasErrors = false;
-        for (const field of fields) {
-            if (formValues[field].length == 0) {
-                errors[field] = true;
-                hasErrors = true;
-            }
-        }
-        if (hasErrors) {
-            setFormErrors({
-                ...formErrors,
-                ...errors
-            });
-            dispatchError("empty fields");
-        } else {
-            setFormValues({
-                name: '',
-                email: '',
-                username: '',
-                password: ''
-            });
-            const data = {
-                username: formValues.username,
-                password: formValues.password,
-                name: formValues.name,
-                email: formValues.email
-            }
-            axios.post('/api/auth/register', data).then((res) => {
-                if (res?.data?.success) {
-                    setIsOpen(true);
-                } else {
-                    dispatchError("account already exists");
+  return (
+    <>
+      <Card
+        className={css({ width: '100%' })}
+      >
+        {
+          <>
+            <FormControl label="Name">
+              <Input
+                value={formValues.name}
+                onChange={e => setFormValues({
+                  ...formValues,
+                  name: e.target.value
+                })}
+                placeholder="Name"
+                clearOnEscape
+                error={formErrors.name}
+                size={inputSize.mini}
+              />
+            </FormControl>
+            <FormControl label="Email">
+              <Input
+                value={formValues.email}
+                onChange={e => setFormValues({
+                  ...formValues,
+                  email: e.target.value
+                })}
+                placeholder="Email"
+                clearOnEscape
+                error={formErrors.email}
+                size={inputSize.mini}
+              />
+            </FormControl>
+            <FormControl label="Username">
+              <Input
+                value={formValues.username}
+                onChange={e => setFormValues({
+                  ...formValues,
+                  username: e.target.value
+                })}
+                placeholder="Username"
+                clearOnEscape
+                error={formErrors.username}
+                size={inputSize.mini}
+              />
+            </FormControl>
+            <FormControl label="Password">
+              <Input
+                value={formValues.password}
+                onChange={e => setFormValues({
+                  ...formValues,
+                  password: e.target.value
+                })}
+                placeholder="Password"
+                clearOnEscape
+                error={formErrors.password}
+                size={inputSize.mini}
+              />
+            </FormControl>
+            <Button
+              onClick={submitRegister}
+              size={buttonSize.mini}
+              overrides={{
+                BaseButton: { 
+                  style: ({ $theme }) => ({ marginRight: '8px' })
                 }
-            }).catch((err) => {
-                console.error(err);
-            });
+              }}
+            >Register</Button>
+            <Button
+              onClick={navigateLogin}
+              size={buttonSize.mini}
+            >Back</Button>
+          </>
         }
-    }
-
-    React.useEffect(() => {
-        if (error.message == "account already exists") {
-            setFormValues({
-                name: '',
-                email: '',
-                username: '',
-                password: ''
-            });
-        }
-        if (!error.error) {
-            setFormErrors({
-                name: false,
-                email: false,
-                password: false,
-                username: false
-            });
-        }
-    }, [error]);
-
-    return (
-        <>
-            <Card
-                className={css({ width: '50%' })}
-            >
-                {
-                    <>
-                        <FormControl label="Name">
-                            <Input
-                                value={formValues.name}
-                                onChange={e => setFormValues({
-                                    ...formValues,
-                                    name: e.target.value
-                                })}
-                                placeholder="Name"
-                                clearOnEscape
-                                error={formErrors.name}
-                                size={inputSize.mini}
-                            />
-                        </FormControl>
-                        <FormControl label="Email">
-                            <Input
-                                value={formValues.email}
-                                onChange={e => setFormValues({
-                                    ...formValues,
-                                    email: e.target.value
-                                })}
-                                placeholder="Email"
-                                clearOnEscape
-                                error={formErrors.email}
-                                size={inputSize.mini}
-                            />
-                        </FormControl>
-                        <FormControl label="Username">
-                            <Input
-                                value={formValues.username}
-                                onChange={e => setFormValues({
-                                    ...formValues,
-                                    username: e.target.value
-                                })}
-                                placeholder="Username"
-                                clearOnEscape
-                                error={formErrors.username}
-                                size={inputSize.mini}
-                            />
-                        </FormControl>
-                        <FormControl label="Password">
-                            <Input
-                                value={formValues.password}
-                                onChange={e => setFormValues({
-                                    ...formValues,
-                                    password: e.target.value
-                                })}
-                                placeholder="Password"
-                                clearOnEscape
-                                error={formErrors.password}
-                                size={inputSize.mini}
-                            />
-                        </FormControl>
-                        <Button
-                            onClick={submitRegister}
-                            size={buttonSize.mini}
-                        >Register</Button>
-                    </>
-                }
-            </Card>
-            <Error />
-            <Modal
-                onClose={closeModal}
-                closeable
-                isOpen={isOpen}
-                animate
-                autoFocus
-                size={SIZE.default}
-                role={ROLE.dialog}
-                unstable_ModalBackdropScroll={true}
-            >
-                <ModalHeader>Success</ModalHeader>
-                <ModalFooter>
-                    <ModalButton onClick={closeModal}>Okay</ModalButton>
-                </ModalFooter>
-            </Modal>
-        </>
-    );
+      </Card>
+      <Error />
+      <Modal
+        onClose={closeModal}
+        closeable
+        isOpen={isOpen}
+        animate
+        autoFocus
+        size={SIZE.default}
+        role={ROLE.dialog}
+        unstable_ModalBackdropScroll={true}
+      >
+        <ModalHeader>Success</ModalHeader>
+        <ModalFooter>
+          <ModalButton onClick={closeModal}>Okay</ModalButton>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
 }
 
 const ConnectedRegister = connect(
-    (state) => {
-        return {
-            error: state.error
-        }
-    },
-    {
-        dispatchError
+  (state) => {
+    return {
+      error: state.error
     }
+  },
+  {
+    dispatchError
+  }
 )(Register);
 
 export default ConnectedRegister;
