cde49a9bbcb98cbe922caf51035fc23195a92598
Benjamin Cash <benjamin.cash@zayo.com>
2022-04-09 16:55:44 -0600

    adding theme

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,4 +1,4 @@
-// constants
+// Constants
 export const RED = 'RED';
 export const ORANGE = 'ORANGE';
 export const YELLOW = 'YELLOW';
@@ -10,15 +10,15 @@ export const PINK = 'PINK';
 export const WHITE = 'WHITE';
 export const BLACK = 'BLACK';
 
-// standard colors
+// Standard Colors
 const PURE_WHITE = '#FFFFFF';
 const PURE_BLACK = '#000000';
 
 // Neon
 const NEON_RED = '#F72119';
 const NEON_ORANGE = '#FF9E3D';
-const NEON_YELLOW = '#FFFF00';
-const NEON_GREEN = '#39FF14';
+const NEON_YELLOW = '#FFF01F';
+const NEON_GREEN = '#1FFF0F';
 const NEON_BLUE = '#4D4DFF';
 const NEON_BLUE_GREEN = '#00F2DE';
 const NEON_PURPLE = '#B026FF';
@@ -39,29 +39,54 @@ const neon = {
   }
 }
 
-// Basic
-const BASIC_RED = '#FF0000';
-const BASIC_ORANGE = '#FFA500';
-const BASIC_YELLOW = '#FFFF00';
-const BASIC_GREEN = '#00FF00';
-const BASIC_BLUE = '#0000FF';
-const BASIC_BLUE_GREEN = '#0D98BA';
-const BASIC_PURPLE = '#6A0DAD';
-const BASIC_PINK = '#FFC0CB';
-const basic = {
-  name: 'Basic',
+// Pastel
+const PASTEL_RED = '#D26466';
+const PASTEL_ORANGE = '#FBBB62';
+const PASTEL_YELLOW = '#F8F1AE';
+const PASTEL_GREEN = '#BEE5B0';
+const PASTEL_BLUE = '#C9D6E8';
+const PASTEL_BLUE_GREEN = '#A5E3E0';
+const PASTEL_PURPLE = '#9A7FAE';
+const PASTEL_PINK = '#F0B6D5';
+const pastel = {
+  name: 'Pastel',
   colors: {
-    [RED]: BASIC_RED,
-    [ORANGE]: BASIC_ORANGE,
-    [YELLOW]: BASIC_YELLOW,
-    [GREEN]: BASIC_GREEN,
-    [BLUE_GREEN]: BASIC_BLUE_GREEN,
-    [BLUE]: BASIC_BLUE,
-    [PURPLE]: BASIC_PURPLE,
-    [PINK]: BASIC_PINK,
+    [RED]: PASTEL_RED,
+    [ORANGE]: PASTEL_ORANGE,
+    [YELLOW]: PASTEL_YELLOW,
+    [GREEN]: PASTEL_GREEN,
+    [BLUE_GREEN]: PASTEL_BLUE_GREEN,
+    [BLUE]: PASTEL_BLUE,
+    [PURPLE]: PASTEL_PURPLE,
+    [PINK]: PASTEL_PINK,
     [WHITE]: PURE_WHITE,
     [BLACK]: PURE_BLACK
   }
 }
 
-export const COLOR_THEMES = [neon, basic];
+// Grey
+const GREY_RED = '#7B797B';
+const GREY_ORANGE = '#918E85';
+const GREY_YELLOW = '#989692';
+const GREY_GREEN = '#B2BEB5';
+const GREY_BLUE = '#708090';
+const GREY_BLUE_GREEN = '#BCC8C6';
+const GREY_PURPLE = '#B1B6B7';
+const GREY_PINK = '#CCCCC4';
+const grey = {
+  name: 'Grey',
+  colors: {
+    [RED]: GREY_RED,
+    [ORANGE]: GREY_ORANGE,
+    [YELLOW]: GREY_YELLOW,
+    [GREEN]: GREY_GREEN,
+    [BLUE_GREEN]: GREY_BLUE_GREEN,
+    [BLUE]: GREY_BLUE,
+    [PURPLE]: GREY_PURPLE,
+    [PINK]: GREY_PINK,
+    [WHITE]: PURE_WHITE,
+    [BLACK]: PURE_BLACK
+  }
+}
+
+export const COLOR_THEMES = [neon, pastel, grey];
