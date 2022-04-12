14ce01ff275397d77eeaaee4de7483eb68986bc8
Benjamin Cash <benjamin.cash@zayo.com>
2022-04-11 22:13:03 -0600

    removing old tests

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,14 +1,13 @@
 const { accessSecret } = require('../config/jwt.config');
 const { verifyToken } = require('../utils/jwt-helpers');
-const isTest = process.env.__TEST__;
 
 module.exports = (req, res, next) => {
-    if(isTest) { // if running tests, set test user id and skip authentication
-        req.auth = {
-            id: '5fb9fe19f50e130570589732'
-        }
-        return next();
-    }
+    // if(isTest) { // if running tests, set test user id and skip authentication
+    //     req.auth = {
+    //         id: '5fb9fe19f50e130570589732'
+    //     }
+    //     return next();
+    // }
     const authHeader = req.headers['auth-token'] || req.headers['authorization'];
     const accessToken = authHeader ? authHeader.split(' ')[1] : false;
     if(!accessToken) return res.status(401).send({
